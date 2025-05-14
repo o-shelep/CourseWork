561fc3fa15e06d8203e1ca35c73d07fcddd58531
shelep-olya <shelep.olya@gmail.com>
2025-05-14 21:17:33 +0300

    chore: initial commit

---
+++ st-mission/src/main/java/com/example/st_mission/StMissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.st_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StMissionApplication.class, args);
+	}
+
+}
---
+++ st-mission/src/test/java/com/example/st_mission/StMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.st_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
