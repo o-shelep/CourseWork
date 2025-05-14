9429bd1148c9d01d266841c6bbd808254c45d97d
o-shelep <shelep.olha@student.uzhnu.edu.ua>
2025-05-14 21:30:38 +0300

    feat: add Role model

---
+++ st-mission/src/main/java/com/example/st_mission/model/ERole.java
@@ -0,0 +1,8 @@
+package com.example.st_mission.model;
+
+public enum ERole {
+    ROLE_ADMIN,
+    ROLE_STUDENT,
+    ROLE_TEACHER
+}
+
---
+++ st-mission/src/main/java/com/example/st_mission/model/RoleEntity.java
@@ -0,0 +1,42 @@
+package com.example.st_mission.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class RoleEntity implements GrantedAuthority {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private ERole name;
+
+    @Override
+    public String getAuthority() {
+        return name.name();
+    }
+
+}
