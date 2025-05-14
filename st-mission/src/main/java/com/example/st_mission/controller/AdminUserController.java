25f7e641d5de78d769ddf2ace8fecc05c621d5c3
o-shelep <shelep.olha@student.uzhnu.edu.ua>
2025-05-14 21:37:07 +0300

    feat: add Admin User controller

---
+++ st-mission/src/main/java/com/example/st_mission/controller/AdminUserController.java
@@ -0,0 +1,57 @@
+package com.example.st_mission.controller;
+
+
+import com.example.st_mission.dto.ChangeRoleDTO;
+import com.example.st_mission.dto.user.MessageResponse;
+import com.example.st_mission.dto.user.UserDTO;
+import com.example.st_mission.dto.user.UserUpdateDTO;
+import com.example.st_mission.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping("/admin/users")
+public class AdminUserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable long id, @RequestBody UserUpdateDTO userUpdateDTO) {
+        UserDTO user = userService.updateUser(id, userUpdateDTO);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponse> deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok(new MessageResponse("Delete user with id " + id + " successful"));
+    }
+
+    @PatchMapping("/{id}/role")
+    public ResponseEntity<UserDTO> changeRole(@PathVariable long id, @RequestBody ChangeRoleDTO dto) {
+        UserDTO user = userService.changeUserRole(id, dto.role());
+        return ResponseEntity.ok(user);
+    }
+
+
+}
